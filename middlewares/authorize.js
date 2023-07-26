d0e34db0753115a9f554774f9cfa9ddbd3c82897
Rahiful <rahifulislam@gmail.com>
2023-07-26 16:04:17 +0600

    update authorization

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,27 +1,21 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET_KEY } = require('../service.json');
-// console.log("enter authorize 1")
 module.exports = async (req, res, next) => {
-    // console.log("enter authorize 2")
-
+    try{
     let token = req.header('Authorization');
-    // console.log("token are:", token)
     if (!token) return res.status(401).send('Access denied! No token provided');
-    //Bearer 1234abcd
-    //[0]=Bearer, [1]= 1234abcd
     else token = token.split(" ")[1].trim();
 
-    const decoded = await jwt.verify(token, JWT_SECRET_KEY);
+    const decoded = jwt.verify(token, JWT_SECRET_KEY);
 
-    // console.log("decoded data are: ", decoded)
-
-    // Decoded => undefind
     if (!decoded) return res.status(400).send('Invalid token');
 
-    //We can uses decoded data any places using req.user
     req.user = decoded;
-    // console.log("req user contains:",  req.user)
-
+    } catch(error){
+        return res.status(401).send({
+            message: error.message
+        });
+    }
 
     next();
 }
